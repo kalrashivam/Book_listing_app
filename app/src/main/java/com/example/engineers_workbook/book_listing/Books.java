5cf4af048bd0cc3662da330583867609aafd6035
engineersworkbook <shivamkalra2013@gmail.com>
2017-11-01 12:17:46 +0530

    Books class created

---
+++ app/src/main/java/com/example/engineers_workbook/book_listing/Books.java
@@ -0,0 +1,34 @@
+package com.example.engineers_workbook.book_listing;
+
+/**
+ * Created by hp on 01-11-2017.
+ */
+
+public class Books {
+
+    private String ntitle;
+
+    private String nprice;
+
+    private String nurl;
+
+
+    books(String name, String price ,String url){
+        ntitle=name;
+        nprice=price;
+        nurl=url;
+    }
+
+
+    public String getNtitle(){
+        return ntitle;
+    }
+
+    public String getNprice(){
+        return nprice;
+    }
+
+    public String getNurl(){
+        return nurl;
+    }
+}
