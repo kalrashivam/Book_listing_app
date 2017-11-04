78ed2321d92b0494ccaf46c8ac0ffc3ca85a5590
engineersworkbook <shivamkalra2013@gmail.com>
2017-11-05 01:21:49 +0530

    made Asynctask class

--- app/src/main/java/com/example/engineers_workbook/book_listing/ListViewActivity.java
+++ app/src/main/java/com/example/engineers_workbook/book_listing/ListViewActivity.java
@@ -1,15 +1,18 @@
 package com.example.engineers_workbook.book_listing;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ListViewActivity extends AppCompatActivity {
 
     public static final String LOG_TAG = ListViewActivity.class.getName();
+    private String First_Url="https://www.googleapis.com/books/v1/volumes?q="
     public String in;
     public Booksadaptor badaptor;
     @Override
@@ -27,5 +30,33 @@ public class ListViewActivity extends AppCompatActivity {
 
         books.setAdapter(badaptor);
 
+
+        BooksAsyncTask task =new BooksAsyncTask();
+        task.execute(First_Url);
+    }
+
+    private class BooksAsyncTask extends AsyncTask<String, void, List<Books>> {
+
+        @Override
+        protected List<Books> doInBackground(String... urls) {
+            if (urls[0] == null || urls.length < 1) {
+                return null;
+            } else {
+                while(int i<urls.length){
+                    List<Books> result = QueryUtils.fetchBooksdata(urls[]);
+                    return result;
+                    i++;
+                }
+            }
+        }
+
+        @Override
+        protected void onPostExecute(List<Books> bookses) {
+
+            badaptor.clear();
+
+            if(bookses != null || !bookses.isEmpty()){
+                badaptor.addAll(bookses);
+        }
     }
 }
